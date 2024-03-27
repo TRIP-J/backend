cb64af9ff21cedc25190d24a12fc16c119b7fd28
zzjiho <youngyoungnim@gmail.com>
2024-03-27 18:02:36 +0900

    Feat: 공통단 에러코드, RestResponse 추가

---
+++ src/main/java/com/tripj/global/code/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.tripj.global.code;
+
+import lombok.Getter;
+
+import static com.tripj.global.code.HttpStatusCode.*;
+
+@Getter
+public enum ErrorCode {
+
+    TEST(INTERNAL_SERVER_ERROR, false, "T001", "business exception test");
+
+    private final HttpStatusCode statusCode;
+    private final boolean notification;
+    private final String code;
+    private final String message;
+
+    ErrorCode(HttpStatusCode statusCode, boolean notification, String code, String message) {
+        this.statusCode = statusCode;
+        this.notification = notification;
+        this.code = code;
+        this.message = message;
+    }
+}
