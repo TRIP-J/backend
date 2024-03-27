9a68fa5101a783221bf4cc77c8c331d802e0336f
eco-kjh <jho101@eco.co.kr>
2024-03-27 15:12:38 +0900

    Init

---
+++ src/main/java/com/tripj/TripjApplication.java
@@ -0,0 +1,13 @@
+package com.tripj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripjApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tripj/api/healthcheck/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.tripj.api.healthcheck;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Hidden
+@Slf4j
+@RestController
+@RequestMapping("/api/admin")
+public class HealthCheckController {
+
+    @GetMapping("/test")
+    public String healthCheck() {
+        log.info("admin test success");
+        return "admin test success";
+    }
+
+}
