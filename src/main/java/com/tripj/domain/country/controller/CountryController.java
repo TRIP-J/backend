edd79ce2d65e29c0f908a41ede2b0975ae93b513
zzjiho <youngyoungnim@gmail.com>
2024-03-29 14:55:36 +0900

    Feat: 나라 조회 api,  Swagger적용

---
+++ src/main/java/com/tripj/domain/country/controller/CountryController.java
@@ -0,0 +1,36 @@
+package com.tripj.domain.country.controller;
+
+import com.tripj.domain.country.model.dto.GetCountryResponse;
+import com.tripj.domain.country.service.CountryService;
+import com.tripj.global.model.RestApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/country")
+@Tag(name = "country", description = "나라 API")
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @Tag(name = "country")
+    @Operation(
+            summary = "나라 조회 API",
+            description = "나라 전체를 조회합니다."
+    )
+    @GetMapping("")
+    public RestApiResponse <List<GetCountryResponse>> getCountry() {
+        return RestApiResponse.success(countryService.getCountry());
+    }
+
+
+
+
+}
---
+++ src/main/java/com/tripj/domain/country/model/dto/GetCountryResponse.java
@@ -0,0 +1,20 @@
+package com.tripj.domain.country.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class GetCountryResponse {
+
+    @Schema(description = "나라명", example = "홍콩", required = true)
+    private String countryName;
+
+    @Schema(description = "나라 코드", example = "HK", required = true)
+    private String countryCode;
+
+    public static GetCountryResponse of(String countryName, String countryCode) {
+        return new GetCountryResponse(countryName, countryCode);
+    }
+}
