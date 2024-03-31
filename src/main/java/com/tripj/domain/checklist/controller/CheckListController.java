9f0f43543d2948a9bdb81bf85521b15df2eb179b
zzjiho <youngyoungnim@gmail.com>
2024-03-31 15:14:38 +0900

    Feat: 체크리스트에 아이템 추가 api

--- src/main/java/com/tripj/domain/checklist/controller/CheckListController.java
+++ src/main/java/com/tripj/domain/checklist/controller/CheckListController.java
@@ -1,14 +1,14 @@
 package com.tripj.domain.checklist.controller;
 
+import com.tripj.domain.checklist.model.dto.CreateCheckListRequest;
+import com.tripj.domain.checklist.model.dto.CreateCheckListResponse;
 import com.tripj.domain.checklist.model.dto.GetCheckListResponse;
 import com.tripj.domain.checklist.service.CheckListService;
 import com.tripj.global.model.RestApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +29,18 @@ public class CheckListController {
     public RestApiResponse <List<GetCheckListResponse>> getCheckList(Long itemCateId,
                                                                      Long userId) {
         return RestApiResponse.success(checkListService.getCheckList(itemCateId, userId));
+    }
+
+
+    @Operation(
+            summary = "체크리스트 추가 API",
+            description = "체크리스트에 아이템을 추가합니다."
+    )
+    @PostMapping("")
+    public RestApiResponse<CreateCheckListResponse> createCheckList(@RequestBody CreateCheckListRequest request,
+                                                                    Long userId) {
+        return RestApiResponse.success(
+                checkListService.createCheckList(request, userId));
     }
 
 
