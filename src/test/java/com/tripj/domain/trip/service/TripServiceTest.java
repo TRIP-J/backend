bf27c986ea2e7edba63e19fda9ca6421f0a1a100
zzjiho <youngyoungnim@gmail.com>
2024-06-20 18:01:30 +0900

    Test: 여행 생성 테스트 코드 작성중

--- src/test/java/com/tripj/domain/trip/service/TripServiceTest.java
+++ src/test/java/com/tripj/domain/trip/service/TripServiceTest.java
@@ -1,27 +1,35 @@
 package com.tripj.domain.trip.service;
 
+import com.tripj.batch.trip.TripBatchScheduler;
+import com.tripj.batch.trip.TripDailyJob;
 import com.tripj.domain.country.model.entity.Country;
 import com.tripj.domain.country.repository.CountryRepository;
 import com.tripj.domain.trip.model.dto.request.CreateTripRequest;
 import com.tripj.domain.trip.model.dto.response.CreateTripResponse;
+import com.tripj.domain.trip.model.entity.Trip;
 import com.tripj.domain.trip.repository.TripRepository;
 import com.tripj.domain.user.constant.Role;
 import com.tripj.domain.user.constant.UserType;
 import com.tripj.domain.user.model.entity.User;
 import com.tripj.domain.user.repository.UserRepository;
+import com.tripj.global.error.exception.BusinessException;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Transactional
 @SpringBootTest
 @ActiveProfiles("test")
 class TripServiceTest {
@@ -34,6 +42,8 @@ class TripServiceTest {
     private UserRepository userRepository;
     @Autowired
     private CountryRepository countryRepository;
+    @Autowired
+    private EntityManager entityManager;
 
     @AfterEach
     void tearDown() {
@@ -44,17 +54,24 @@ class TripServiceTest {
 
     @Test
     @DisplayName("여행 계획을 최초 등록한다.")
-    void createTrip_whenBeforeExistingTrip() {
-        // given
-        CreateTripRequest createTripRequest = createTripRequest();
-        User user = createUser();
-        userRepository.save(user);
+    void createTripWhenBeforeExistingTrip() {
 
+        // given
         Country country = createCountry();
         countryRepository.save(country);
 
+        User user = createUser();
+        userRepository.save(user);
+
+        CreateTripRequest createTripRequest =
+                createTripRequest(
+                        "NOW",
+                        country.getId(),
+                        LocalDate.of(2022, 10, 1),
+                        LocalDate.of(2022, 10, 10));
+
         // when
-        CreateTripResponse trip = tripService.createTrip(createTripRequest, 1L);
+        CreateTripResponse trip = tripService.createTrip(createTripRequest, user.getId());
 
         // then
         assertNotNull(trip);
@@ -65,21 +82,118 @@ class TripServiceTest {
         assertEquals(LocalDate.of(2022, 10, 10), trip.getEndDate());
     }
 
+    @Test
+    void 여행_계획은_EndDate가_끝나기_전까지_한개밖에_생성하지_못한다() {
 
-    private CreateTripRequest createTripRequest() {
+        // given
+        Country country = createCountry();
+        countryRepository.save(country);
+
+        User user = createUser();
+        userRepository.save(user);
+
+        CreateTripRequest createTripRequest =
+                createTripRequest(
+                        "NOW",
+                        country.getId(),
+                        LocalDate.of(2022, 10, 1),
+                        LocalDate.of(2022, 10, 10));
+        tripService.createTrip(createTripRequest, user.getId());
+
+        // when // then
+        assertThatThrownBy(() -> tripService.createTrip(createTripRequest, user.getId()))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("이미 생성한 여행이 있습니다.");
+    }
+
+    @Test
+    void EndDate가_지나면_여행_계획의_Previous는_B01_로_업데이트_된다() {
+
+        // given
+        Country country = createCountry();
+        countryRepository.save(country);
+
+        User user = createUser();
+        userRepository.save(user);
+
+        CreateTripRequest createTripRequest =
+                createTripRequest(
+                        "NOW",
+                        country.getId(),
+                        LocalDate.of(2022, 10, 1),
+                        LocalDate.now());
+        CreateTripResponse trip = tripService.createTrip(createTripRequest, user.getId());
+
+        // when
+        tripService.changeTripPrevious();
+
+        // then
+        Optional<Trip> updatedTrip = tripRepository.findById(trip.getTripId());
+        assertNotNull(updatedTrip);
+        assertEquals("즐거운 오사카 여행", updatedTrip.get().getTripName());
+        assertEquals("여행", updatedTrip.get().getPurpose());
+        assertEquals("B01", updatedTrip.get().getPrevious());
+    }
+
+    @Test
+    void EndDate가_지나면_여행_계획의_Previous는_B01에서_순차적으로_증가한다() {
+
+        // given
+        Country country = createCountry();
+        countryRepository.save(country);
+
+        User user = createUser();
+        userRepository.save(user);
+
+        CreateTripRequest createTripRequest =
+                createTripRequest(
+                        "NOW",
+                        country.getId(),
+                        LocalDate.of(2022, 10, 1),
+                        LocalDate.now());
+        CreateTripResponse trip = tripService.createTrip(createTripRequest, user.getId());
+
+        tripService.changeTripPrevious();
+
+        CreateTripRequest createTripRequest2 =
+                createTripRequest(
+                        "NOW",
+                        country.getId(),
+                        LocalDate.of(2022, 10, 1),
+                        LocalDate.now());
+
+        CreateTripResponse trip2 = tripService.createTrip(createTripRequest2, user.getId());
+
+        // when
+        tripService.changeTripPrevious();
+
+        // then
+        Optional<Trip> updatedTrip = tripRepository.findById(trip2.getTripId());
+        assertNotNull(updatedTrip);
+        assertEquals("즐거운 오사카 여행", updatedTrip.get().getTripName());
+        assertEquals("여행", updatedTrip.get().getPurpose());
+        assertEquals("B02", updatedTrip.get().getPrevious());
+    }
+
+
+
+
+
+
+
+    private CreateTripRequest createTripRequest(String previous, Long countryId, LocalDate startDate, LocalDate endDate) {
         return CreateTripRequest.builder()
                 .tripName("즐거운 오사카 여행")
                 .purpose("여행")
-                .previous("NOW")
-                .startDate(LocalDate.of(2022, 10, 1))
-                .endDate(LocalDate.of(2022, 10, 10))
-                .countryId(1L)
+                .previous(previous)
+                .startDate(startDate)
+                .endDate(endDate)
+                .countryId(countryId)
                 .build();
     }
 
     private User createUser() {
         return User.builder()
-                .id(1L)
                 .userType(UserType.KAKAO)
                 .email("asdf@naver.com")
                 .nickname("다람지기엽지")
@@ -90,7 +204,6 @@ class TripServiceTest {
 
     private Country createCountry() {
         return Country.builder()
-                .id(1L)
                 .name("일본")
                 .code("JP")
                 .build();
